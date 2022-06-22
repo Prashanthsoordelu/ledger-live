1a8219a331c62e933feba85c48c0d544df60a0d0
Pavan Vora <pavanvora.cs@gmail.com>
2022-06-22 15:50:35 +0530

    add minimum required cardanoApp version

--- libs/ledger-live-common/src/apps/support.ts
+++ libs/ledger-live-common/src/apps/support.ts
@@ -35,6 +35,7 @@ const appVersionsRequired = {
   Elrond: ">= 1.0.11",
   Ethereum: ">= 1.9.17",
   Solana: ">= 1.2.0",
+  Cardano: ">=4.1.0",
 };
 export function mustUpgrade(
   deviceModel: DeviceModelId,
